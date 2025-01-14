9f66206e6b8da859ca9b2110d089b646063ba778
bilalabidkhan <bilalabid6578@gmail.com>
2025-01-14 23:32:15 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full bg-slate-400 pt-16 pb-24">
+    <div className="w-full h-screen bg-slate-400 pt-16 pb-24">
        <div className="text-center pt-28">
          <button className="border rounded-xl pt-4 pb-4 pl-2 pr-2 lg:pl-10 lg:pr-10 bg-black text-white hover:bg-white hover:text-black">
           <Link href={"/clientside"}>Client Side Data</Link>
